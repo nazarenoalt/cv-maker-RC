a056b8dd21b65f8aa27d063f493fe034c03a62e4
nazarenoalt <nazalt256@gmail.com>
2021-11-29 18:18:26 -0300

    [UPDATE] created db file to segment data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 // Container components
 import Header from './containers/Header'
 import Sidebar from './containers/Sidebar'
@@ -18,149 +18,38 @@ import Hobbies from './components/Hobbies'
 import { Wrapper } from "./App.style"
 import './normalize.css'
 import PrintPdfButton from "./components/PrintPdfButton"
+// Data
+import data from "./db"
 
 const App = () => {
+  const initialState = {...data}
+  const [state, setState] = useState(initialState);
+  
   return (
     <Wrapper>
       <PrintPdfButton />
       <Header>
-        <HeaderProfile userInfo={{
-          name: "Nazareno",
-          surname: "Aznar Altamiranda",
-          jobTitle: "Frontend | React Developer Jr."
-        }} />
+        <HeaderProfile userInfo={state.header} />
       </Header>
 
       <Sidebar>
-        
         <div className="Sidebar__personal-info-section">
-           <AboutMe 
-          text="I stay in constantly learning, ductile and focused in team work. I am looking forward to improve my developer skills with new challenging projects. My main focus is to improve and facilitate the access to technology and information to the population, creating intuitive interfaces and comfortable experiences for any user."
-        />
-          <Contact data={[
-            {
-              title:"Cellphone",
-              content:"(+54) 3644 724359"
-            },
-            {
-              title:"Email",
-              content:"nazalt256@gmail.com"
-            }
-          ]} />
-          <SocialNetworks 
-            data={[
-              {
-                title:"Github",
-                content:"github.com/nazarenoalt"
-              },
-              {
-                title:"Linkedin",
-                content:"linkedin.com/in/nazarenoalt"
-              },
-              {
-                title:"Platzi",
-                content:"platzi.com/p/nazarenoalt"
-              }
-            ]} 
-        />
+          <AboutMe text={state.header.text} />
+          <Contact data={state.contact} />
+          <SocialNetworks  data={state.SocialNetworks} />
         </div>
-        <Skills 
-          hardSkills={["Javascript", "HTML & CSS", "React", "MongoDB", "Webpack", "Babel","Git & Github", "NPM", "Bootstrap", "JSON/API managment"]}
-          softSkills={["Assertive communication", "Oratory", "Critical thinking"]}
-        />
-        <Hobbies 
-          hobbies={["Violoncello", "Running & strength exercises", "Cooking", "Finances & economy", "Cryptocurrencies"]}  
-        />
+
+        <Skills hardSkills={state.hardSkills} softSkills={state.softSkills} />
+        <Hobbies hobbies={state.hobbies} />
         <Footer />
       </Sidebar>
 
       <Body>
-       
-        <Education 
-          data={[
-            {
-              title: "High School",
-              institute:"UEP N°82 \"Hogar de Bethania\"",
-              description:"Saenz Peña, Chaco",
-              date:"2016",
-            },
-            {
-              title: "Frontend Development",
-              institute:"Platzi",
-              description:"Web development school & Javascript School.",
-              date:"Oct. 2020 - present",
-            },
-            {
-              title: "English",
-              institute:"Platzi",
-              description:"English School",
-              date:"Oct. 2020 - present",
-            }
-          ]}
-        />
-        <Languages 
-          data={[
-             {
-              title:"Spanish",
-              content:"Native",
-            },
-            {
-              title:"English",
-              content:"Advance - B2",
-            },
-          ]}
-        />
-        
-        <Repository 
-          data={[
-            {
-              "title": "Bright Academy",
-              "content": "Project for English teacher: Education platform, with login system, posts, token authentication. - Javascript, HTML and CSS, consuming a rest API",
-              "link": "github.com/nazarenoalt/bright-academy",
-            },
-            {
-              "title": "Pokemon Battle",
-              "content": "Pokemon battle simulator - Javascript, HTML and CSS, consuming a rest API.",
-              "link": "github.com/nazarenoalt/pokemon-battle",
-            },
-            {
-              "title": "Piano Virtual SM",
-              "content": "Virtual piano that emulate 'Simon says' game. - Javascript, HTML y CSS.",
-              "link": "github.com/nazarenoalt/piano-virtual-SM",
-            },
-            {
-              "title": "React Shop",
-              "content": "Ecommerce app - React.js, HTML and CSS, Webpack, Babel. Consuming a rest API.",
-              "link": "github.com/nazarenoalt/react-shop",
-            },
-            {
-              "title": "Simply React TODO",
-              "content": "Todo app. - React.js, HTML and CSS, LocalStorage.",
-              "link": "github.com/nazarenoalt/simply-react-todo",
-            },
-            {
-              "title": "Interactive Price Component",
-              "content": "Interactive page to choose product prices. - Javascript, HTML and CSS, consuming a rest API.",
-              "link": "github.com/nazarenoalt/interactive-pricing-component",
-            },
-            {
-              "title": "Crowdfundfing Product Page",
-              "content": "Landing Page with crowdfunding interface. - Javascript, HTML and CSS, consuming a rest API. ",
-              "link": "github.com/nazarenoalt/crowdfunding-product-page",
-            },
-            {
-              "title": "Platziconf Bootstrap",
-              "content": "Bootstrap landing page. - Javascript, HTML, CSS, Bootstrap.",
-              "link": "github.com/nazarenoalt/simply-react-todo",
-            },
-            {
-              "title": "Curriculum (Mobile First)",
-              "content": "First version of my curriculum in landing page format, which becomes a normal CV in desktop resolution. - HTML y CSS.",
-              "link": "github.com/nazarenoalt/curriculum",
-            },
-          ]}
-        />
+        <Education data={state.education} />
+        <Languages data={state.languages} />
+        <Repository data={state.repository} />
       </Body>
+      
     </Wrapper>
   )
 }
---
+++ src/db.js
@@ -0,0 +1,115 @@
+const data = {
+  //header
+  header: {
+    name: "Nazareno",
+    surname: "Aznar Altamiranda",
+    jobTitle: "Frontend | React Developer Jr.",
+    text: "I stay in constantly learning, ductile and focused in team work. I am looking forward to improve my developer skills with new challenging projects. My main focus is to improve and facilitate the access to technology and information to the population, creating intuitive interfaces and comfortable experiences for any user.",
+  }, 
+  contact: [
+    {
+      title:"Cellphone",
+      content:"(+54) 3644 724359"
+    },
+    {
+      title:"Email",
+      content:"nazalt256@gmail.com"
+    },
+  ],
+  SocialNetworks: [
+    {
+      title:"Github",
+      content:"github.com/nazarenoalt"
+    },
+    {
+      title:"Linkedin",
+      content:"linkedin.com/in/nazarenoalt"
+    },
+    {
+      title:"Platzi",
+      content:"platzi.com/p/nazarenoalt"
+    },
+  ],
+  hardSkills: ["Javascript", "HTML & CSS", "React", "MongoDB", "Webpack", "Babel","Git & Github", "NPM", "Bootstrap", "JSON/API managment"],
+  softSkills: ["Assertive communication", "Oratory", "Critical thinking"],
+  hobbies: ["Violoncello", "Running & strength exercises", "Cooking", "Finances & economy", "Cryptocurrencies"],
+  education: [
+    {
+      title: "High School",
+      institute:"UEP N°82 \"Hogar de Bethania\"",
+      description:"Saenz Peña, Chaco",
+      date:"2016",
+    },
+    {
+      title: "Frontend Development",
+      institute:"Platzi",
+      description:"Web development school & Javascript School.",
+      date:"Oct. 2020 - present",
+    },
+    {
+      title: "English",
+      institute:"Platzi",
+      description:"English School",
+      date:"Oct. 2020 - present",
+    }
+  ],
+  languages: [
+    {
+      title:"Spanish",
+      content:"Native",
+    },
+    {
+      title:"English",
+      content:"Advance - B2",
+    },
+],
+repository: [
+  {
+    "title": "Bright Academy",
+    "content": "Project for English teacher: Education platform, with login system, posts, token authentication. - Javascript, HTML and CSS, consuming a rest API",
+    "link": "github.com/nazarenoalt/bright-academy",
+  },
+  {
+    "title": "Pokemon Battle",
+    "content": "Pokemon battle simulator - Javascript, HTML and CSS, consuming a rest API.",
+    "link": "github.com/nazarenoalt/pokemon-battle",
+  },
+  {
+    "title": "Piano Virtual SM",
+    "content": "Virtual piano that emulate 'Simon says' game. - Javascript, HTML y CSS.",
+    "link": "github.com/nazarenoalt/piano-virtual-SM",
+  },
+  {
+    "title": "React Shop",
+    "content": "Ecommerce app - React.js, HTML and CSS, Webpack, Babel. Consuming a rest API.",
+    "link": "github.com/nazarenoalt/react-shop",
+  },
+  {
+    "title": "Simply React TODO",
+    "content": "Todo app. - React.js, HTML and CSS, LocalStorage.",
+    "link": "github.com/nazarenoalt/simply-react-todo",
+  },
+  {
+    "title": "Interactive Price Component",
+    "content": "Interactive page to choose product prices. - Javascript, HTML and CSS, consuming a rest API.",
+    "link": "github.com/nazarenoalt/interactive-pricing-component",
+  },
+  {
+    "title": "Crowdfundfing Product Page",
+    "content": "Landing Page with crowdfunding interface. - Javascript, HTML and CSS, consuming a rest API. ",
+    "link": "github.com/nazarenoalt/crowdfunding-product-page",
+  },
+  {
+    "title": "Platziconf Bootstrap",
+    "content": "Bootstrap landing page. - Javascript, HTML, CSS, Bootstrap.",
+    "link": "github.com/nazarenoalt/simply-react-todo",
+  },
+  {
+    "title": "Curriculum (Mobile First)",
+    "content": "First version of my curriculum in landing page format, which becomes a normal CV in desktop resolution. - HTML y CSS.",
+    "link": "github.com/nazarenoalt/curriculum",
+  },
+]
+}
+
+export default data
