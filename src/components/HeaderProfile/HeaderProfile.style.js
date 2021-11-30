10cd423ace8bf5326c158999afd1ac3f87612916
nazarenoalt <nazalt256@gmail.com>
2021-11-30 14:02:28 -0300

    [REFACTOR] headerProfile classNames

--- src/components/HeaderProfile/HeaderProfile.style.js
+++ src/components/HeaderProfile/HeaderProfile.style.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  .Header__title {
+  .title {
     letter-spacing: .3rem;
     font-size: 3rem;
     line-height: .7;
     color: var(--red)
     }
 
-  .Header__jobTitle {
+  .jobTitle {
     font-size: 1.5rem;
     font-weight: bold;
   }
--- src/components/HeaderProfile/index.js
+++ src/components/HeaderProfile/index.js
@@ -4,9 +4,9 @@ import { Wrapper } from './HeaderProfile.style'
 const HeaderProfile = ({userInfo}) => {
   return (
     <Wrapper>
-      <h1 className="Header__title">{userInfo.name}</h1>
-      <h1 className="Header__title">{userInfo.surname}</h1>
-      <p className="Header__jobTitle">{userInfo.jobTitle}</p>
+      <h1 className="title">{userInfo.name}</h1>
+      <h1 className="title">{userInfo.surname}</h1>
+      <p className="jobTitle">{userInfo.jobTitle}</p>
     </Wrapper>
   )
 }
