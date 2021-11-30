d68c9754adffb1e1c0a23eecae30b7af9d900758
nazarenoalt <nazalt256@gmail.com>
2021-11-30 14:01:00 -0300

    [REFACTOR] footer classNames

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  .Footer {
   width: 100%;
   text-align: center;
   margin-top: 100%;
-  }
 
-  .Footer small {
+  small {
     font-size: 1rem;
   }
 
-  .Footer a {
+  a {
     color: var(--sidebar-color);
     font-weight: bold;
     text-decoration: underline;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,11 @@ import { Wrapper } from './Footer.style.js'
 const Footer = () => {
   return (
     <Wrapper>
-      <div className="Footer">
         <small>
           Curriculum created in React by <a href="https://www.github.com/nazarenoalt" target="_blank">@nazarenoalt</a>
           <br />
           <a href={`https://www.github.com/nazarenoalt/curriculum-v2`} target="_blank">sourcecode</a>
         </small>
-      </div>
     </Wrapper>
   )
 }
