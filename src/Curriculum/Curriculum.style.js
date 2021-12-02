2d2d987aba051223a5f2d76afbd3d5769c0507c3
nazarenoalt <nazalt256@gmail.com>
2021-12-02 18:04:08 -0300

    [FIX]

--- src/Curriculum/Curriculum.style.js
+++ src/Curriculum/Curriculum.style.js
@@ -1,11 +1,17 @@
 import styled from "styled-components"
 
+/* Next thing to do is to replace font-size value by the width of parent component */
+
 export const Wrapper = styled.div`
-  font-size: ${((document.querySelector("body").offsetWidth / 2) / 100) * 1.5}px;
+
+  font-size: ${((document.querySelector("body").offsetWidth /2) / 100) * 1.5}px;
   display: grid;
   grid-template-areas: "sidebar header header"
-    "sidebar body body"
-    "sidebar body body";
+                       "sidebar body body"
+                       "sidebar body body";
+
+  grid-template-columns  : repeat(3, 1fr) ;
+
   .Sidebar {
     grid-area: sidebar;
   }
